fa488d6516af45e4387844d0836a233c2c53d207
swatiashinde <swatishinde749@gmail.com>
2020-06-16 16:27:11 +0530

    commit

---
+++ JenkinsDemo/src/main/java/Capgi/JenkinsDemo/App.java
@@ -0,0 +1,13 @@
+package Capgi.JenkinsDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
